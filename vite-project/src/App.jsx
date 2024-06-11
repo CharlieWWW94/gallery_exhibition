6d1bfe84bba3da3100faf344ee8d1d07a17285a3
Lananh Nguyen <lananhnguyen@live.co.uk>
2024-06-11 19:19:09 +0100

    grid with css grid and pics

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Grid from './components/Grid'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className="App">
-        <Navbar/>
+        <div className="App-header">
+          <Navbar/>
+        </div>
+          <Grid/>
       </div>
       </>
   )
